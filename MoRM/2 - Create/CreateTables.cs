bf80e6fb4033d47048fe591d543d1481d16d2b45
Matthew Almeida <matthew.almeida572@gmail.com>
2020-08-26 23:10:10 -0300

    creating table method

--- MoRM/2 - Create/CreateTables.cs
+++ MoRM/2 - Create/CreateTables.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using MoRM._1___Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace MoRM
 {
     public class CreateTables
     {
+        public string CreateTable(string TableName, List<ColumnEntity> ColumnObj)
+        {
+            string BaseQuery = $"CREATE TABLE @{Regex.Replace(TableName, @"[^0-9a-zA-Z:,]+", "")}( ";
+            
+            var Query = new StringBuilder();
+
+            Query.Append(BaseQuery);
+
+            foreach(var Column in ColumnObj)
+            {
+                string ColumnQuery = $"@{Regex.Replace(Column.ColumnName, @"[^0-9a-zA-Z:,]+", "")} @{Regex.Replace(Column.ColumnType, @"[^0-9a-zA-Z:,]+", "")}(@{Column.ColumnSize})";
+                Query.Append(ColumnQuery);
+            }
+
+            return Query.ToString();
+        }
     }
 }
