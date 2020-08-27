65fb7ee06f3ecc4dd3183071ab890f6fb59bcf90
Matthew Almeida <matthew.almeida572@gmail.com>
2020-08-26 22:52:35 -0300

    making column entity

---
+++ MoRM/1 - Entity/ColumnEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoRM._1___Entity
+{
+    public class ColumnEntity
+    {
+        public string ColumnName { get; set; }
+        public string ColumnType { get; set; }
+        public int ColumnSize { get; set; }
+    }
+}
