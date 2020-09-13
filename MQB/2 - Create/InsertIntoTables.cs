b1776358319334606b71b0fb66d2ae68f08ad421
Matthew Almeida <55630599+Lamadelrae@users.noreply.github.com>
2020-09-13 20:33:13 -0300

    setting insert, will finish next week

--- MQB/2 - Create/InsertIntoTables.cs
+++ MQB/2 - Create/InsertIntoTables.cs
@@ -1,13 +1,25 @@
-﻿using System;
+﻿using MQB.Entity;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MQB.Create
 {
-    public class InsertIntoTables
+    public static class InsertIntoTables
     {
+        public static SqlCommand InsertIntoTable(this MQBMain Table)
+        {
+            try
+            {
 
+            }
+            catch(Exception)
+            {
+
+            }
+        }
     }
 }
